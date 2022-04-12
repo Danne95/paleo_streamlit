5908a13ebd4bca306e1d6edad8a4f7fde73274bb
paleo_server <historicalmanuscripts@historicalmanuscripts.europe-west1-b.c.sce-innercloud.internal>
2022-04-12 10:43:07 +0000

    initialization of server files

---
+++ main.py
@@ -0,0 +1,31 @@
+
+#terminal:
+#streamlit run main.py
+#streamlit run main.py --server.port=80
+
+import streamlit as st
+from PIL import Image
+import pandas as pd
+import numpy as np
+import keras as keras
+import tensorflow as tf
+
+
+def load_image(image_file):
+    img = Image.open(image_file)
+    return img
+
+st.set_page_config(page_title="Palaeography Classification", page_icon=":crystal_ball:", layout="wide")
+st.title("Welcome to our paleo web application.!!!")
+st.write("site under construction!")
+st.write("[Learn more >](https://en.wikipedia.org/wiki/Palaeography)")
+menu = ["None","Image"]
+choice = st.sidebar.selectbox("Menu",menu)
+if choice == "Image":
+    image_file = st.file_uploader("Upload Image:", type=["png","jpg","jpeg"])
+    if image_file is not None:
+        # To See details
+        file_details = {"filename":image_file.name, "filetype":image_file.type, "filesize":image_file.size}
+        st.write(file_details)
+        # To View Uploaded Image
+        st.image(load_image(image_file))
