0284a932ae647a0d6a12a9f3a1e01c65daa34e47
paleo_server <historicalmanuscripts@historicalmanuscripts.europe-west1-b.c.sce-innercloud.internal>
2022-04-16 19:36:28 +0000

    add dependemcies but 1GB model was removed sadly

--- main.py
+++ main.py
@@ -3,6 +3,8 @@
 #streamlit run main.py
 #streamlit run main.py --server.port=80
 
+#pipreqs ./ --force #for requirements
+
 #git pull
 #git add .
 #git commit -m "text"
@@ -12,11 +14,12 @@ from unittest import result
 import streamlit as st
 from PIL import Image
 import pandas as pd
+import tensorflow as tf
 import keras
 from predict import predict_image
 
-class_model = keras.models.load_model("/class_model")
-subclass_model = keras.models.load_model("/subclass_model")
+class_model = keras.models.load_model("/home/historicalmanuscripts/paleo_site/keras_models_v1/Priya_Dwivedi_classes_v1_10e_05d")
+subclass_model = keras.models.load_model("/home/historicalmanuscripts/paleo_site/keras_models_v1/Priya_Dwivedi_subclasses_v1_10e_05d")
 
 def load_image(image_file):
     img = Image.open(image_file)
